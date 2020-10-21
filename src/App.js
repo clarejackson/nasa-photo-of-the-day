a1e4a977dddeb50f96f34ab7a37c4b80f1d329a4
Clare Jackson <clare-jackson@lambdastudents.com>
2020-10-20 21:29:44 -0400

    Axios complete. Files created.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
+import axios from "axios"
+// import Card from "./Card"
+
+
+const apiKey = '2mkShLeBVBTh55jagY5zd59w6G8uzGGURddMWKoH';
+
 
 function App() {
+const [photo, setPhoto] = useState([]);
+  useEffect(() => {
+    axios
+      .get(`https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&page=1&api_key=${apiKey}`)
+      .then(response => {
+        console.log(response);
+        setPhoto(response.data)
+    })
+    .catch(error => {
+      console.log('the data was not returned', error)
+    })
+  }, [])
+  
   return (
+    
     <div className="App">
       <p>
         Read through the instructions in the README.md file to build your NASA
@@ -10,6 +30,6 @@ function App() {
       </p>
     </div>
   );
-}
+  }
 
 export default App;
